b4ce2477c51dcc0cd9be747c2d7a390b4c9ff58f
Roman <rmianni@gmail.com>
2023-02-06 18:47:37 -0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -140,4 +140,22 @@
   $("[data-paroller-factor]").paroller();
 
 
+  $(window).scroll(function () {
+
+    var bottom_of_window = $(window).scrollTop() + $(window).height();
+    //fade-in
+    $('.fade-ani').each(function () {
+      var bottom_of_object = $(this).position().top + $(this).outerHeight();
+      if (bottom_of_window > bottom_of_object) {
+        $(this).addClass('showing');
+      }
+      else {
+        $(this).removeClass('showing');
+      }
+    });
+
+  });
+
+
+
 }())
